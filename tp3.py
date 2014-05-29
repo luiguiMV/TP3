4ee9a5fc729c61957fadd7597c7c80972a53c91a
luiguiMV <luiguimv198@hotmail.com>
2014-05-29 01:08:33 -0600

    tp3, templates, uploads

---
+++ tp3.py
@@ -0,0 +1,40 @@
+""" Tarea Programa 3,
+	Realizado por:  Andres Fernadez,
+					Luigui Madrigal,
+					Jose Maria Rojas"""
+
+import os
+from flask import Flask, render_template, request, redirect, url_for, abort, send_from_directory
+from werkzeug import secure_filename
+
+app = Flask("Sml")#instancia de la aplicacion web con Flask
+app.config['UPLOAD_FOLDER'] = 'uploads/'#carpeta donde se subira el archivo
+app.config['ALLOWED_EXTENSIONS'] = set(['sml','png','jpg'])
+
+def allowed_file(filename):
+	return '.' in filename and \
+			filename.rsplit('.',1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+#pagina inicial
+@app.route('/')
+def home():
+	return render_template('index.html')
+	
+#ruta donde se procede el archivo subido
+@app.route('/upload', methods=['POST'])
+def upload():
+	file = request.files['file']#obtiene el nombre del archivo
+	if file and allowed_file(file.filename):#verifica que el archivo tenga una extension valida
+		filename = secure_filename(file.filename)#convierte el archivo en seguro
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))#guarda el archivo en la carpeta 
+		return redirect(url_for('uploaded_file', filename=filename))#redirecciona hacia la pagina para mostrar
+	
+#ruta que espera un parametro con el nombre del archivo.
+#luego lo muestra en el browser	
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+	
+#ejecuta la app
+if __name__ == '__main__':
+    app.run()#host='192.168.10.103')
