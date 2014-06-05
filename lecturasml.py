d8ed809de2ea2c729e7c24f02a756620ee9853c1
jrojascarrilo <rojascrc@hotmail.com>
2014-06-05 04:48:58 -0600

    integracion de funciones de luigui con el codigo existente

--- lecturasml.py
+++ lecturasml.py
@@ -1,6 +1,6 @@
 import re
 listalectura=[]#Lista que almacena todas las lineas leidas en el archivo SML en sublistas
-valores=[["x",56],["y",2]] #Lista que se almacena todos los valores y resultados para mostrar el resultado de estatico y dinamico
+valores=[] #Lista que se almacena todos los valores y resultados para mostrar el resultado de estatico y dinamico
 
 ######################funcion para leer el archivo SML#####################################
 def leerSML():
@@ -34,48 +34,56 @@ def procesar():
 		while indice<len(i):
 			bandera=determinarBandera(i)
 			if i[indice] == 'val':
-				indice=procesarVal(i,indice+1)						
+				indice=procesarVal(i,indice+1)
+			
+						
 			indice+=1
 			
-def asociarvalor():
-	return 0
-	
 def procesarVal(i,contador):
 	temp=[]
+	temp2=0 #Valor que almancena los valores temporales de las operaciones aritmeticas hasta que llegue al ;
 	temp.append(i[contador])
 	contador+=1
 	if i[contador] == "=":
 		contador+=1
-		if tipodato(i[contador]) != "string":
-			temp.append(i[contador])
-			valores.append(temp)
-			if i[contador]=="(":
+		while i[contador]!=";":
+			if tipodato(i[contador])!=str and tipodato(i[contador])!="signo" and tipodato(i[contador])!="variable":
+				temp2=eval(i[contador])
 				contador+=1
-				temp=[]
-				while i[contador]!=")":
-					temp.append(i[contador])
-					contador+=1
+			elif tipodato(i[contador])=="signo" and tipodato(i[contador+1])!=str:
+				if tipodato(i[contador+1])=="variable":
+					temp2=evaluarExpresion(temp2,i[contador],getValor(i[contador+1]))
+					contador+=2
+				else:
+					temp2=evaluarExpresion(temp2,i[contador],eval(i[contador+1]))
+					contador+=2
+			elif tipodato(i[contador])=="variable":
+				temp2=getValor(i[contador])
+				contador+=1	
+			else:
 				contador+=1
+				
+		temp.append(temp2) #guarda el resultado final de la operacion aritmetica en temp
+		valores.append(temp)	
 	return contador
-	
-def evaluarExpresion(p1, operador, p2):#funcion que retorna la evaluacion de la expresion
+####################funcion que retorna la evaluacion de la expresion##############################33	
+def evaluarExpresion(p1, operador, p2):
 	if operador == '+':
 		return p1+p2
 	elif operador == '~':
 		return p1-p2
 	elif operador == '*':
 		return p1*p2
-	elif operador == '/':
+	elif operador == 'div':
 		return p1/p2
 	
-######################
+######################Obterner el valor de la variable##################################
 def getValor(variable):
 	for i in valores:
 		if i[0] == variable:
-			print i[1]
-			return
-	print "Variable "+variable+" no definida"
-
+			return i[1]
+			
+	return "Variable "+variable+" no definida"
 ######################Funcion para crear una bandera auxiliar para cuando vienen varios parentesis en un Val###############################
 def determinarBandera(i):
         bandera=0
@@ -99,22 +107,24 @@ def resultado():
 ####################################Funcin que analiza el tipo de dato que contiene los elementos dentro de valor##############################
 def tipodato(dato):
 	try:
-		if type(eval(dato))==int:
-			return "int"
+		if dato=="+" or dato=="~" or dato=="div" or dato=="*":
+			return "signo"
+		elif dato=="true" or dato=="false":
+			return bool
+		elif type(eval(dato))==int:
+			return int
 		elif type(eval(dato))==float:
-			return "float"
+			return float
 		elif type(eval(dato))==tuple:
-			return "tuple"
+			return tuple
 		elif type(eval(dato))==list:
-			return "list"
-		elif type(eval(dato))==bool:
-			return "bool"
+			return list
 		else:
-			return "string"
+			return str
 	except NameError:
-		return "string"
+		return "variable"
 	except SyntaxError:
-		return "string"
+		return str
 ####################################Funcion de prueba para estar leyendo el contenido de las listas#####################################			
 def imprimir():
 	for i in listalectura:
@@ -130,8 +140,7 @@ def numero(strnum):
 		print "Error"
 		
 		   
-#leerSML()
+leerSML()
 #procesar()
-#getValor("z");
-#imprimir()
+imprimir()
 #determinarBandera(["val","=","(","2","+","3",")","+","(","3","*",")"])
